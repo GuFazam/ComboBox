ea86b4af42a0dcc42169903a4e5e6559c4635332
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-03-03 21:43:59 -0300

    Adicionar arquivos de projeto.

---
+++ ComboBox/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbEscolha = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCliqueAqui = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbEscolha
+            // 
+            this.cmbEscolha.FormattingEnabled = true;
+            this.cmbEscolha.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.cmbEscolha.Location = new System.Drawing.Point(235, 69);
+            this.cmbEscolha.Name = "cmbEscolha";
+            this.cmbEscolha.Size = new System.Drawing.Size(121, 21);
+            this.cmbEscolha.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Escolha um número de 1 à 12:";
+            // 
+            // btnCliqueAqui
+            // 
+            this.btnCliqueAqui.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCliqueAqui.Location = new System.Drawing.Point(362, 69);
+            this.btnCliqueAqui.Name = "btnCliqueAqui";
+            this.btnCliqueAqui.Size = new System.Drawing.Size(205, 23);
+            this.btnCliqueAqui.TabIndex = 2;
+            this.btnCliqueAqui.Text = "Escolha um número e clique aqui!";
+            this.btnCliqueAqui.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(502, 194);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(65, 25);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(108, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(677, 320);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCliqueAqui);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbEscolha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbEscolha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCliqueAqui;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
